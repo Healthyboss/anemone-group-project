b9d481fd1c5c101b5dce134ff1202e016c3aa46d
Healthyboss <150171938+Healthyboss@users.noreply.github.com>
2024-01-27 20:44:31 +0000

    Deploying to gh-pages from @ Healthyboss/anemone-group-project@72757488a785dbab34769bcd4aa67cdbe178a8fd 🚀

---
+++ index.d2d4fbad.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[subscribe-modal-open]"),closeModalBtn:document.querySelector("[subscribe-modal-close]"),modal:document.querySelector("[subscribe-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.d2d4fbad.js.map
--- index.f6a5a50b.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-open2]"),closeModalBtn:document.querySelector("[data-modal-close2]"),modal:document.querySelector("[data-modal2]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.f6a5a50b.js.map
